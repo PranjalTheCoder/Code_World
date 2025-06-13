9dbd252bf79d94c0d7d60817abcca167083148be
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-06-13 14:06:45 +0530

    Add: user online/offline status indicators

---
+++ server/utils/actions.js
@@ -0,0 +1,15 @@
+const ACTIONS = {
+	JOIN: "join",
+	JOINED: "joined",
+	DISCONNECTED: "disconnected",
+	SYNC_FILES: "sync-files",
+	UPDATE_CLIENTS_LIST: "update-clients-list",
+	FILE_CREATED: "file-created",
+	FILE_UPDATED: "file-updated",
+	FILE_RENAMED: "file-renamed",
+	FILE_DELETED: "file-deleted",
+	OFFLINE: "offline",
+	ONLINE: "online",
+}
+
+module.exports = ACTIONS
