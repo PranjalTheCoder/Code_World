f0c281feefce6f95af9ddc261ee7cb39d4dfb380
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-06-13 12:01:55 +0530

    add: public deploy dir

---
+++ client/src/context/AppProvider.tsx
@@ -0,0 +1,14 @@
+
+
+
+function AppProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col h-screen">
+            <div className="flex-1 overflow-y-auto">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default AppProvider;
