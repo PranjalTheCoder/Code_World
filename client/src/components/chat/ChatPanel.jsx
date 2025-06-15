94c1bdbde37725e43cf13caa321afc8449afbcb2
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-06-15 13:33:34 +0530

    add: New message notification, save last scroll position

--- client/src/components/chat/ChatPanel.jsx
+++ client/src/components/chat/ChatPanel.jsx
@@ -1,59 +1,73 @@
-import PropTypes from "prop-types"
-import { useContext, useRef } from "react"
-import { IoSend } from "react-icons/io5"
+import { useContext, useEffect, useRef } from "react"
 import AppContext from "../../context/AppContext"
-import ACTIONS from "../../utils/actions"
-import { formatDate } from "../../utils/formateDate"
+import ChatContext from "../../context/ChatContext"
+import ChatInput from "./ChatInput"
 
-function ChatInput({ setMessages, messagesContainerRef }) {
-    const { socket, clients, roomId } = useContext(AppContext)
-    const inputRef = useRef(null)
+function ChatPanel() {
+    const {
+        messages,
+        isNewMessage,
+        setIsNewMessage,
+        lastScrollHeight,
+        setLastScrollHeight,
+    } = useContext(ChatContext)
+    const { socket } = useContext(AppContext)
+    const messagesContainerRef = useRef(null)
 
-    const handleSendMessage = (e) => {
-        e.preventDefault()
+    // Scroll to bottom when messages change
+    useEffect(() => {
+        messagesContainerRef.current.scrollTop =
+            messagesContainerRef.current.scrollHeight
+    }, [messages])
 
-        const inputVal = inputRef.current.value.trim()
-        const client = clients.find((client) => client.socketId === socket.id)
-
-        if (inputVal.length > 0) {
-            const message = {
-                message: inputVal,
-                username: client?.username || "Unknown",
-                socketId: socket.id,
-                timestamp: formatDate(new Date()),
-            }
-            socket.emit(ACTIONS.SEND_MESSAGE, { roomId, message })
-            setMessages((messages) => [...messages, message])
-            inputRef.current.value = ""
-            messagesContainerRef.current.scrollTop =
-                messagesContainerRef.current.scrollHeight
+    useEffect(() => {
+        if (isNewMessage) {
+            setIsNewMessage(false)
         }
+        messagesContainerRef.current.scrollTop = lastScrollHeight
+    }, [isNewMessage, setIsNewMessage, lastScrollHeight])
+
+    const handleScroll = (e) => {
+        setLastScrollHeight(e.target.scrollTop)
     }
 
     return (
-        <form
-            onSubmit={handleSendMessage}
-            className="flex justify-between rounded-md border border-primary"
-        >
-            <input
-                type="text"
-                className="w-full flex-grow rounded-md border-none bg-dark p-2 outline-none"
-                placeholder="Enter a message..."
-                ref={inputRef}
-            />
-            <button
-                className="flex items-center justify-center rounded-r-md bg-primary p-2 text-dark"
-                type="submit"
+        <div className="tab-height flex max-h-full min-h-[500px] w-full flex-col gap-4 p-4">
+            <h1 className="text-base">Group Chat</h1>
+            <div
+                className="flex-grow overflow-auto rounded-md border-t-4 border-primary bg-darkHover p-2"
+                ref={messagesContainerRef}
+                onScroll={handleScroll}
             >
-                <IoSend size={24} />
-            </button>
-        </form>
+                {/* Chat messages */}
+                {messages.map((message, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className={
+                                "mb-2 w-[80%] self-end break-words rounded-md bg-dark px-3 py-2" +
+                                (message.socketId === socket.id
+                                    ? " ml-auto "
+                                    : "")
+                            }
+                        >
+                            <div className="flex justify-between">
+                                <span className="text-xs text-primary">
+                                    {message.username}
+                                </span>
+                                <span className="text-xs text-white">
+                                    {message.timestamp}
+                                </span>
+                            </div>
+                            <p className="py-1">{message.message}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            {/* Chat input */}
+            <ChatInput />
+        </div>
     )
 }
 
-ChatInput.propTypes = {
-    setMessages: PropTypes.func.isRequired,
-    messagesContainerRef: PropTypes.object.isRequired,
-}
-
-export default ChatInput
+export default ChatPanel
